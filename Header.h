815588e220f92341f3dcd36954b518da8edfb404
arichorn <78001398+arichorn@users.noreply.github.com>
2023-06-11 08:55:55 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -27,6 +27,7 @@
 #import "Tweaks/YouTubeHeader/YTAlertView.h"
 #import "Tweaks/YouTubeHeader/YTISectionListRenderer.h"
 #import "Tweaks/YouTubeHeader/YTPivotBarItemView.h"
+#import "Tweaks/YouTubeHeader/ASDisplayNode.h" // LowContrastMode
 #import "Tweaks/YouTubeHeader/ELMContainerNode.h" // LowContrastMode
 #import "Tweaks/YouTubeHeader/ELMTextNode.h" // LowContrastMode
 
